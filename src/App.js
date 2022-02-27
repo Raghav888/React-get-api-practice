ef7434138fa1b8ccf7f0ae7aa64fefdf1a7edaa1
Raghav Bang <raghavbang2012@gmail.com>
2022-02-27 20:09:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  let [data, setdata] = useState([]);
+  let [status, statussetter] = useState(false);
+  const loadData = async () => {
+    try {
+      setdata("");
+      statussetter(true);
+      const recevieData = await axios.get("/api/users");
+      setdata(recevieData.data.users);
+      statussetter(false);
+    } catch {
+      console.log("Error");
+      statussetter(false);
+    }
+  };
+  return (
+    <div className="App">
+      <h1> Data </h1>
+      <button onClick={loadData}> Click to load data from server </button>
+      {status && <h2>Loading..</h2>}
+      <div>{data && data.map((items) => <li>{items.name}</li>)}</div>
+    </div>
+  );
+}
